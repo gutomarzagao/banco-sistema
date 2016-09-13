f1d694afebfa8837cc72202d9d737215b37f75b0
João Victor Gelio <jvgelio@gmail.com>
2016-09-13 00:20:40 -0300

    simplificando o codigo

--- banco.py
+++ banco.py
@@ -1,10 +1,7 @@
-comando = input("Digite um comando:")
-while (comando!="sair"):
+comando = ""
+while (True):
+    comando = input("Digite um comando:")
     print(comando)
     if(comando!='sair'):
         print("comando inexistente")
-        comando = input("Digite um comando:")
-
-else:
-    exit()
-
+    else: break
