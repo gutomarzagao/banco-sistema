a8e4db99b8fc7f89778a064d719d49e066b38498
João Victor Gelio <jvgelio@gmail.com>
2016-09-13 17:26:32 -0300

    funcoes depositar, sacar e saldo

--- banco.py
+++ banco.py
@@ -1,7 +1,24 @@
 comando = ""
+saldo=[0]
 while (True):
     comando = input("Digite um comando:")
     print(comando)
-    if(comando!='sair'):
+    if(comando=="deposito"):
+        depositar=int(input("Valor do depósito:"))
+        if(depositar<0):
+            print("nao aceitamos depositos negativos")
+        else:
+            saldo.append(depositar)
+    elif(comando=="saque"):
+        sacar=int(input("Valor que deseja sacar:"))
+        if(sacar<0):
+            print("nao aceitamos saques negativos")
+        else:
+            saldo.append(-sacar)
+    elif(comando=="saldo"):
+        print("seu saldo é", sum(saldo))
+    elif(comando=="sair"):
+        exit()
+    elif(comando!=['sair', 'saldo', 'saque', 'deposito']):
         print("comando inexistente")
     else: break
