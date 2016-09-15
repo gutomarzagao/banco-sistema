6521fef3bf52e8e507e0fb3bcd9220230f69ac89
João Victor Gelio <jvgelio@gmail.com>
2016-09-15 00:34:45 -0300

    funcao extrato

--- banco.py
+++ banco.py
@@ -1,29 +1,33 @@
-comando = ""
+import funcoes
+
 extrato=[0]
 saldo=0
 depositar=int()
 while (True):
     comando = input("Digite um comando:")
     print(comando)
-    if(comando=="deposito"):
+    if comando=="deposito":
         depositar=int(input("Valor do depósito:"))
-        if(depositar<0):
-            print("nao aceitamos depositos negativos.")
+        if depositar<0:
+            print('nao aceitamos depositos negativos.')
         else:
             extrato.append(depositar)
             saldo += depositar
-    elif(comando=="saque"):
+    elif comando== "saque":
         sacar=int(input("Valor que deseja sacar:"))
         if(sacar<0):
             print("nao aceitamos saques negativos.")
-        elif(sacar>saldo):
+        elif sacar>saldo:
             print("saque maior que saldo, operacao nao realizada.")
         else:
             extrato.append(-sacar)
             saldo -= sacar
-    elif(comando=="saldo"):
+    elif comando== "saldo":
         print("seu saldo é", saldo)
-    elif(comando=="sair"):
+    elif comando == "extrato":
+        for k,v in enumerate(extrato):
+            print(k, v)
+    elif comando== "sair":
         break
     else:
         print("comando inexistente")
