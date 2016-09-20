4c4a39898085ad4fff875562a3b2e5281e97d24a
João Victor Gelio <jvgelio@gmail.com>
2016-09-19 21:46:11 -0300

    codigo refatorado corrigindo funcoes

--- main.py
+++ main.py
@@ -1,51 +1,63 @@
-import conta
-
-extrato=[0]
+extrato=[]
 saldo=0
+operacao = []
+
+# aqui comecam as funcoes
 def comandar(x):
-    comando == x
-    return
+    return comando == x
 def exibir_saldo():
     print("seu saldo é", saldo)
-    return
 
 def exibir_extrato():
-    return
-
-def sacar():
-    return
+    for x,y in zip(extrato,operacao):
+        print (y,x)
 
 def depositar():
-    return
+    global saldo
+    depositar = int(input("Valor do depósito:"))
+    if depositar < 0:
+        print('nao aceitamos depositos negativos.')
+    else:
+        extrato.append(depositar)
+        operacao.append("deposito")
+        saldo += depositar
+def sacar():
+    global saldo
+    sacar = int(input("Valor que deseja sacar:"))
+    if (sacar < 0):
+        print("nao aceitamos saques negativos.")
+    elif (sacar > saldo):
+        print("saque maior que saldo, operacao nao realizada.")
+    else:
+        extrato.append(-sacar)
+        operacao.append("saque")
+        saldo -= sacar
+
+# aqui comeca o codigo de verdade
 while (True):
     comando = input("Digite um comando:")
     print(comando)
+    if comandar('entrar'):
+        print("Bem vindo a sua conta")
+        while (True):
+            comando = input("Digite um comando:")
+            print(comando)
+            if comandar('deposito'):
+                depositar()
 
-    if comando=="deposito":
-        depositar=int(input("Valor do depósito:"))
-        if depositar<0:
-            print('nao aceitamos depositos negativos.')
-        else:
-            extrato.append(depositar)
-            saldo += depositar
+            elif comandar('saque'):
+                sacar()
 
-    elif comando== "saque":
-        sacar=int(input("Valor que deseja sacar:"))
-        if(sacar<0):
-            print("nao aceitamos saques negativos.")
-        elif sacar>saldo:
-            print("saque maior que saldo, operacao nao realizada.")
-        else:
-            extrato.append(-sacar)
-            saldo -= sacar
+            elif comandar('saldo'):
+                exibir_saldo()
 
-    elif comandar('saldo'):
-        exibir_saldo()
-
-    elif comando == "extrato":
-        for k,v in enumerate(extrato):
-            print(k, v)
-    elif comando== "sair":
+            elif comandar('extrato'):
+                exibir_extrato()
+            elif comandar('sair'):
+                break
+            else:
+                print("comando inexistente")
+    elif comandar('fechar'):
         break
     else:
-        print("comando inexistente")
+        print("faca o login para fazer operacoes")
