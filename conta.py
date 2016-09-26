48b664097fe1d3c6298dedfb377257f7731d392a
João Victor Gelio <jvgelio@gmail.com>
2016-09-25 21:12:46 -0300

    criada classe conta, codigo refatorado

---
+++ conta.py
@@ -0,0 +1,32 @@
+class Conta:
+    extrato = []
+    saldo = 0
+    operacao = []
+    valor = 0
+
+
+
+    def exibir_saldo(self):
+        print("seu saldo é", self.saldo)
+
+    def exibir_extrato(self):
+        for x,y in zip(self.extrato,self.operacao):
+            print (y,x)
+
+    def depositar(self, valor):
+        if valor < 0:
+            print('nao aceitamos depositos negativos.')
+        else:
+            self.extrato.append(valor)
+            self.operacao.append("deposito")
+            self.saldo += valor
+
+    def sacar(self, valor):
+        if (valor < 0):
+            print("nao aceitamos saques negativos.")
+        elif (valor > self.saldo):
+            print("saque maior que saldo, operacao nao realizada.")
+        else:
+            self.extrato.append(-valor)
+            self.operacao.append("saque")
+            self.saldo -= valor
--- main.py
+++ main.py
@@ -1,63 +1,31 @@
-extrato=[]
-saldo=0
-operacao = []
-
-# aqui comecam as funcoes
-def comandar(x):
-    return comando == x
-def exibir_saldo():
-    print("seu saldo é", saldo)
-
-def exibir_extrato():
-    for x,y in zip(extrato,operacao):
-        print (y,x)
-
-def depositar():
-    global saldo
-    depositar = int(input("Valor do depósito:"))
-    if depositar < 0:
-        print('nao aceitamos depositos negativos.')
-    else:
-        extrato.append(depositar)
-        operacao.append("deposito")
-        saldo += depositar
-def sacar():
-    global saldo
-    sacar = int(input("Valor que deseja sacar:"))
-    if (sacar < 0):
-        print("nao aceitamos saques negativos.")
-    elif (sacar > saldo):
-        print("saque maior que saldo, operacao nao realizada.")
-    else:
-        extrato.append(-sacar)
-        operacao.append("saque")
-        saldo -= sacar
-
+from conta import Conta
+conta1 = Conta()
 # aqui comeca o codigo de verdade
-while (True):
-    comando = input("Digite um comando:")
-    print(comando)
-    if comandar('entrar'):
-        print("Bem vindo a sua conta")
-        while (True):
-            comando = input("Digite um comando:")
-            print(comando)
-            if comandar('deposito'):
-                depositar()
+comando = input("faça login:")
+print(comando)
+if comando == 'entrar':
+    print("Bem vindo a sua conta")
+    while (True):
+        comando = input("Digite um comando:")
+        print(comando)
+        if comando == 'deposito':
+            valor = int(input("Valor do depósito:"))
+            conta1.depositar(valor)
 
-            elif comandar('saque'):
-                sacar()
+        elif comando == 'saque':
+            valor = int(input("Valor do saque:"))
+            conta1.sacar(valor)
 
-            elif comandar('saldo'):
-                exibir_saldo()
+        elif comando == 'saldo':
+            conta1.exibir_saldo()
 
-            elif comandar('extrato'):
-                exibir_extrato()
-            elif comandar('sair'):
-                break
-            else:
-                print("comando inexistente")
-    elif comandar('fechar'):
-        break
-    else:
-        print("faca o login para fazer operacoes")
+        elif comando == 'extrato':
+            conta1.exibir_extrato()
+        elif comando == 'sair':
+            break
+        else:
+            print("comando inexistente")
+elif comando == 'fechar':
+    exit()
+else:
+    print("faca o login para fazer operacoes")
